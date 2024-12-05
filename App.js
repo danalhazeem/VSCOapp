da54f8c91f761b170750ea1c72e03eeb64f0facf
danalhazeem <92695924+danalhazeem@users.noreply.github.com>
2024-12-05 08:42:11 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,43 +1,20 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-
-import CameraScreen from './screens/CameraScreen';
-import GalleryScreen from './screens/GalleryScreen';
-import EditScreen from './screens/EditScreen';
-import ProfileScreen from './screens/ProfileScreen';
-
-const Tab = createBottomTabNavigator();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === 'Camera') {
-              iconName = focused ? 'camera' : 'camera-outline';
-            } else if (route.name === 'Gallery') {
-              iconName = focused ? 'images' : 'images-outline';
-            } else if (route.name === 'Edit') {
-              iconName = focused ? 'brush' : 'brush-outline';
-            } else if (route.name === 'Profile') {
-              iconName = focused ? 'person' : 'person-outline';
-            }
-
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-      >
-        <Tab.Screen name="Camera" component={CameraScreen} />
-        <Tab.Screen name="Gallery" component={GalleryScreen} />
-        <Tab.Screen name="Edit" component={EditScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
